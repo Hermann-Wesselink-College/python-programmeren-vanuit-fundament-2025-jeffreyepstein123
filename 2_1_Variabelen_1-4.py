d355f00f28e9b1817337b4b741904b225de2f1d4
jeffreyepstein123 <37133@lln.hethwc.nl>
2025-09-18 07:23:45 +0000

    modified:   2_6_Variabelen_1-5.py

--- 2_1_Variabelen_1-4.py
+++
@@ -1,1 +0,0 @@
-# opgaven 1 tot en met 4 van paragraaf 2.1 Variabelen
--- 2_6_Variabelen_1-5.py
+++ 2_6_Variabelen_1-5.py
@@ -1,1 +1,20 @@
-# opdracht 1 tpt en met 5 van paragraaf 2.6
+naam = "Piet"
+print (naam.lower())
+
+postcode = "1188DM"
+print (postcode.lower())
+
+woonplaats = "Amstelveen"
+print (woonplaats.upper())
+
+verzendmethode = "PostNL"
+betaalmethode = "iDEAL"
+print (verzendmethode.lower())
+print (betaalmethode.upper())
+
+invoer_postcode = input("Wat is je postcode? ")
+print(invoer_postcode.upper())
+
+naam = "Gasselternijveenschemond"
+print (len(naam))
+
