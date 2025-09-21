484058d528afcc17ffcbeb36f26896dc8aae5525
jeffreyepstein123 <37133@lln.hethwc.nl>
2025-09-21 14:22:56 +0000

    Changes to be committed:

--- 2_8_Variabelen_1-3.py
+++ 2_8_Variabelen_1-3.py
@@ -1,1 +1,17 @@
-# opdrachten 1 tot en met 3 van pragraaf 2.8
+prijs = 24.49
+print('Prijs: ' + str(prijs))
+
+voornaam = 'Sjaak'
+achternaam = 'Heuvels'
+print('Mijn naam is ' + voornaam + ' ' + achternaam)
+print('Mijn naam bestaat uit ' + str(len(voornaam + ' ' + achternaam)) + ' letters')
+print(voornaam.lower())
+print(achternaam.upper())
+
+voornaam = input("Wat is je voornaam? ")
+achternaam = input("Wat is je achternaam? ")
+
+naam = voornaam + " " + achternaam
+
+print("Je naam is: " + naam)
+print("Je naam bestaat uit " + str(len(naam)) + " letters (inclusief spaties)")
